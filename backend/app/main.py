f4f83d466dc70fa4262ffe9641785be9e8002733
Kerjing <jing2003038@gmail.com>
2026-08-16 08:44:41 +0800

    add icon

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
     allow_origins=[
         "https://laughing-eureka-q5jjx476qwgc4xjp-3000.app.github.dev",
         "http://localhost:3000",
+        "https://notedr.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
